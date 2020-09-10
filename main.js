c1af9b593ffba55dc0132f77868db41b59c608fa
Anshul-creation <66676986+Anshul-creation@users.noreply.github.com>
2020-09-10 16:40:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const alphabets = document.querySelectorAll('.choice')
+
+
+alphabets.forEach(choice => choice.addEventListener('click', function (){
+	alert(`you clicked on ${choice.id}`);
+}));
+
+
+
+$(window).load(function(){
+	var options = ["Kiwi" , "Watermelon" , "Mango" , "Apple" , "Banana" , "Cherry" , "Cranberry" , "Grape" , "Orange"];
+	const ul = document.getElementById("box");
+	
+	for (var i = 0; i <options.length; i++){
+	var option = options[i];
+	
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(option));
+    li.className = "ui-screen-hidden";
+    li.innerHTML = `<a href="#" class="ui-btn ui-btn-icon-right ui-icon-carat-r"> ${option} </a>`;
+
+	ul.appendChild(li);}
+	const list = document.querySelectorAll("a");
+
+    list.forEach(a => a.addEventListener('click' , function(){
+	alert (`you cicked on ${a.text}`);
+}));
+});
+
