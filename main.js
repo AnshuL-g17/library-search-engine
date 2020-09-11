623eaf108a828386686491d5c12aa01c28cf070a
Saket Juneja <saket.juneja@levensystems.com>
2020-09-11 11:11:03 -0400

    indentation

--- main.js
+++ main.js
@@ -1,47 +1,46 @@
 var options = [
-	"Kiwi",
-	"Watermelon",
-	"Mango",
-	"Apple",
-	"Banana",
-	"Cherry",
-	"Cranberry",
-	"Grape",
-	"Orange",
+  "Kiwi",
+  "Watermelon",
+  "Mango",
+  "Apple",
+  "Banana",
+  "Cherry",
+  "Cranberry",
+  "Grape",
+  "Orange",
 ];
 
 $(window).load(function () {
-	initialize();
+  initialize();
 });
 
-function initialize(){
-	initializeAlphabets();
-	initializeSearch();
+function initialize() {
+  initializeAlphabets();
+  initializeSearch();
 }
 
-function initializeAlphabets(){
-	$('.choice').click(function(e){
-		displayMessage(e.target.innerHTML);
-	});
+function initializeAlphabets() {
+  $(".choice").click(function (e) {
+    displayMessage(e.target.innerHTML);
+  });
 }
 
-function initializeSearch(){
-	initializeSearchItems();
-	$('#box li a').click(function(e){
-		displayMessage(e.target.innerHTML);
-	});
+function initializeSearch() {
+  initializeSearchItems();
+  $("#box li a").click(function (e) {
+    displayMessage(e.target.innerHTML);
+  });
 }
 
-function initializeSearchItems(){
-	for (var i = 0; i < options.length; i++) {
-		$('#box').append(`
+function initializeSearchItems() {
+  for (var i = 0; i < options.length; i++) {
+    $("#box").append(`
 			<li class="ui-screen-hidden">
 				<a href="#" class="ui-btn ui-btn-icon-right ui-icon-carat-r">${options[i]}</a>
-			</li>`
-		);
-	}
+			</li>`);
+  }
 }
 
-function displayMessage(message){
-	alert(`You clicked on ${message}`);
+function displayMessage(message) {
+  alert(`You clicked on ${message}`);
 }
