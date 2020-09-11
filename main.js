e582884e01c2430bf9e04e6bd9e387b7a98da3b8
Saket Juneja <saket.juneja@levensystems.com>
2020-09-11 11:05:46 -0400

    refactored code

--- main.js
+++ main.js
@@ -1,35 +1,47 @@
-const alphabets = document.querySelectorAll('.choice')
+var options = [
+	"Kiwi",
+	"Watermelon",
+	"Mango",
+	"Apple",
+	"Banana",
+	"Cherry",
+	"Cranberry",
+	"Grape",
+	"Orange",
+];
 
-
-alphabets.forEach(choice => choice.addEventListener('click', function (){
-	alert(`you clicked on ${choice.id}`);
-}));
-
-
-
-$(window).load(function(){
-	var options = ["Kiwi" , "Watermelon" , "Mango" , "Apple" , "Banana" , "Cherry" , "Cranberry" , "Grape" , "Orange"];
-	const ul = document.getElementById("box");
-	
-	for (var i = 0; i <options.length; i++){
-	var option = options[i];
-	
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(option));
-    li.className = "ui-screen-hidden";
-    li.innerHTML = `<a href="#" class="ui-btn ui-btn-icon-right ui-icon-carat-r"> ${option} </a>`;
-
-	ul.appendChild(li);}
-	const list = document.querySelectorAll("a");
-
-    list.forEach(a => a.addEventListener('click' , function(){
-	alert (`you cicked on ${a.text}`);
-}));
-	
-	const clear = document.querySelectorAll("ui-input-clear ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all ui-input-clear-hidden");;
-
-    clear.EventListener('click' , function(){
-	alert (`you cicked on ${a.text}`);
-});
+$(window).load(function () {
+	initialize();
 });
 
+function initialize(){
+	initializeAlphabets();
+	initializeSearch();
+}
+
+function initializeAlphabets(){
+	$('.choice').click(function(e){
+		displayMessage(e.target.innerHTML);
+	});
+}
+
+function initializeSearch(){
+	initializeSearchItems();
+	$('#box li a').click(function(e){
+		displayMessage(e.target.innerHTML);
+	});
+}
+
+function initializeSearchItems(){
+	for (var i = 0; i < options.length; i++) {
+		$('#box').append(`
+			<li class="ui-screen-hidden">
+				<a href="#" class="ui-btn ui-btn-icon-right ui-icon-carat-r">${options[i]}</a>
+			</li>`
+		);
+	}
+}
+
+function displayMessage(message){
+	alert(`You clicked on ${message}`);
+}
